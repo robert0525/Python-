609c7b76c06fea2cfdefbc72dcd63fa51fbbde67
Robert Rydlewski <robertrydlewski1984@gmail.com>
2020-02-20 21:26:03 +0100

    first comit

---
+++ hello.py
@@ -0,0 +1,6 @@
+first_name = input("What's is your first name?  ")
+print("Hello", first_name)
+if first_name == "Robert":
+    print(first_name, "is learning Python")
+print("Have a greate day {}!".format(first_name))
+      
