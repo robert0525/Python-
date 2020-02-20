202657f6fcdeb77676c597adb946fa44f06825bb
Robert Rydlewski <robertrydlewski1984@gmail.com>
2020-02-20 22:13:24 +0100

    fixing bug

--- hello.py
+++ hello.py
@@ -2,7 +2,9 @@ first_name = input("What's is your first name?  ")
 print("Hello", first_name)
 if first_name == "Robert":
     print(first_name, "is learning Python")
+elif first_name == "Maxi":
+    print(first_name, " is learning with fellow students in the Comunity! Me too!")
 else: 
-    print("You should totally learn Python, {}!'.format(first_name))
+    print("You should totally learn Python, {}!".format(first_name))
 print("Have a greate day {}!".format(first_name))
        
