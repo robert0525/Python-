7781962d3db75589548927cbc643373a1c9c8a43
Robert Rydlewski <robertrydlewski1984@gmail.com>
2020-02-20 21:56:19 +0100

    conditional statment PY

--- hello.py
+++ hello.py
@@ -2,5 +2,7 @@ first_name = input("What's is your first name?  ")
 print("Hello", first_name)
 if first_name == "Robert":
     print(first_name, "is learning Python")
+else: 
+    print("You should totally learn Python, {}!'.format(first_name))
 print("Have a greate day {}!".format(first_name))
-      
+       
